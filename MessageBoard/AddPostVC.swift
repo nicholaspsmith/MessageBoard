406cd8e7a22c6ac00adaaaf39477bc4d2b65bd0b
Nick P Smith <npsmith1990@gmail.com>
2015-12-29 18:52:27 -0700

    Restructure to more MVC friendly format

---
+++ MessageBoard/AddPostVC.swift
@@ -0,0 +1,19 @@
+//
+//  AddPostVC.swift
+//  MessageBoard
+//
+//  Created by Nicholas Smith on 12/29/15.
+//  Copyright © 2015 Nick. All rights reserved.
+//
+
+import UIKit
+
+class AddPostVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
