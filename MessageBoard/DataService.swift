cd478ad09dee74cdc7f7a589fc1bf40e40e31a83
Nicholas Smith <npsmith1990@gmail.com>
2016-01-05 23:02:14 -0600

    Create DataService Class

---
+++ MessageBoard/DataService.swift
@@ -0,0 +1,14 @@
+//
+//  DataService.swift
+//  MessageBoard
+//
+//  Created by Nick on 1/5/16.
+//  Copyright © 2016 Nick. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    // static - only 1 instance will exist
+    static let instance = DataService()
+}
