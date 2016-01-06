22ba321746fd8318ee9f15d287495805a7247756
Nicholas Smith <npsmith1990@gmail.com>
2016-01-05 21:41:19 -0600

    Implement image picker

--- MessageBoard/AddPostVC.swift
+++ MessageBoard/AddPostVC.swift
@@ -21,19 +21,28 @@ class AddPostVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
         super.viewDidLoad()
         
         postImg.layer.cornerRadius = postImg.frame.size.width / 2
-        addPicBtn.layer.cornerRadius = addPicBtn.frame.size.width / 2
         
         imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
     }
 
     @IBAction func addPicBtnPressed(sender: UIButton!) {
         sender.setTitle("", forState: .Normal)
+        presentViewController(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func makePostBtnPressed(sender: AnyObject) {
+        if let title = titleField.text, let desc = descField.text, let img = postImg.image {
+            
+        }
     }
     
     @IBAction func cancelBtnPressed(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        postImg.image = image
     }
 }
