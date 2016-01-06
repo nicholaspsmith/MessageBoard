a87243f0545fdd38eddf040e53a45ce5e971a875
Nicholas Smith <npsmith1990@gmail.com>
2016-01-05 23:06:05 -0600

    Create method stubs

--- MessageBoard/DataService.swift
+++ MessageBoard/DataService.swift
@@ -7,8 +7,35 @@
 //
 
 import Foundation
+import UIKit
 
 class DataService {
     // static - only 1 instance will exist
     static let instance = DataService()
+    
+    private var _loadedPosts = [Post]()
+    
+    var loadedPosts: [Post] {
+        return _loadedPosts
+    }
+    
+    func savePosts() {
+        
+    }
+    
+    func loadPosts() {
+        
+    }
+    
+    func saveImageAndCreatePath(image: UIImage) {
+        
+    }
+    
+    func imageForPath(path: String) {
+        
+    }
+    
+    func addPost(post: Post) {
+        
+    }
 }
